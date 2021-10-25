17c0acf41e1f9301d6085b971a2e6b6565db1e61
Gregory Schmidt <gregory.b.schmidt@hotmail.com>
2021-10-24 21:10:17 -0800

    Remove specific keypad event

---
+++ WyzeSenseCore/WyzeKeyPadState.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WyzeSenseCore
+{
+
+    public enum WyzeKeyPadState: byte
+    {
+        Inactive = 0x00,
+        Active = 0x01,
+        Disarmed = 0x02,
+        Home = 0x03,
+        Away = 0x04,
+        Alarm = 0x05,
+        Unknown
+    }
+}
